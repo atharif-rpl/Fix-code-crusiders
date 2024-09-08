d5bfd1cb9ed5a922e01b40bf24fccdc8f728e0c2
atharif-rpl <atha55733@gmail.com>
2024-09-08 17:16:51 +0700

    first and last

---
+++ script.js
@@ -0,0 +1,95 @@
+// // Get the modal
+// var modal = document.getElementById("loginModal");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("loginBtn");
+
+// // // Get the <span> element that closes the modal
+// // var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+
+// // Get the modal
+// var modal = document.getElementById("containerbabi");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("loginBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {}
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// // }
+
+
+
+
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('containerbabi');
+
+// signUpButton.addEventListener('click', () => {
+// 	container.classList.add("right-panel-active");
+// });
+
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
+
+
+// Get the modal
+var modal = document.getElementById("loginModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
